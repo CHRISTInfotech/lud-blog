6ef622fde961290dd52273d3e7e89ba2b214194a
alwin-97 <alwinjosephkaduppil@gmail.com>
2024-07-08 15:03:52 +0530

    fixing

--- ludblog/wsgi.py
+++ ludblog/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'letusdream.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ludblog.settings')
 
 application = get_wsgi_application()
