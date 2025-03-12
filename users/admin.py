4d370a83ca054c60117918cd8bf4bb1a9a110e1f
jaise george <jaise.george@msds.christuniversity.in>
2025-03-12 18:34:35 +0530

    otp bug fixed

--- users/admin.py
+++ users/admin.py
@@ -1,1 +1,9 @@
 from django.contrib import admin
+
+from django.apps import apps
+
+models = apps.get_app_config('users').get_models()
+
+for model in models:
+    if model not in admin.site._registry:  # Check if the model is already registered
+        admin.site.register(model)
